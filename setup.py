9e1cce8d3d99f745757976b0d91f2952b4f5e17d
Donn <me@donn.website>
2023-09-05 15:10:40 +0300

    fix build_py

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ from setuptools.command.build_py import build_py as _build_py
 module_name = "libparse"
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
-subprocess.check_call(["make", "patch"], cwd=__dir__)
-
 ext = Extension(
     name="_libparse",
     swig_opts=["-c++"],
@@ -22,16 +20,16 @@ ext = Extension(
     extra_compile_args=["-std=c++11", "-DFILTERLIB"],
 )
 
-
 class build_py(_build_py):
-    def run(self):
+    def run(self) -> None:
+        subprocess.check_call(["make", "patch"], cwd=__dir__)
         self.run_command("build_ext")
         return super().run()
-
+    
 setup(
     name=module_name,
     packages=["libparse"],
-    version="0.1.4",
+    version="0.1.5",
     description="Python wrapper around Yosys' libparse module",
     long_description=open("Readme.md").read(),
     long_description_content_type="text/markdown",
@@ -47,6 +45,7 @@ setup(
     ],
     python_requires=">3.6",
     ext_modules=[ext],
-    build_py=build_py,
-    package_data = {'': ['libparse.py']}  # needed for sdist
+    cmdclass={
+        'build_py': build_py,
+    },
 )
